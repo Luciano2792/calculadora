03ccb2b79976ddd74530a5da2e2a7b4815531e62
Luciano2792 <adono2002@hotmail.com>
2018-06-30 15:45:01 -0500

    Terminado

--- calculator.js
+++ calculator.js
@@ -1,18 +1,22 @@
 var operacion = prompt("Que operacion deseas usar?");
 
-if (operacion == "volumen de un cilindro") {
+if (operacion === "volumen de un cilindro") {
 	var radio = prompt("Cual es el radio del cilindro?");
 	var altura = prompt("Cual es la altura del cilindro?");
 	var exponente = radio *= radio;
 	var resultado_pi = exponente *= 3.141592;
 	var resultado = resultado_pi *= altura;
-	alert("El volumen del cilindro es " + resultado);
+	alert("El volumen del cilindro es: " + resultado);
 }
-if (operacion == "transformar de onzas a toneladas"); {
+if (operacion === "transformar de onzas a toneladas") {
 	var onzas = prompt("Cuantas onzas quieres transformar?");
 	var onz_tn = onzas * 0.0000283495; 
-	alert(onzas + " onzas equivalen a " + onz_tn + " toneladas");
+	alert(onzas + " onzas equivalen a: " + onz_tn + " toneladas");
 }
-if (operacion == "factorizacion de un numero") {
+if (operacion === "factorizacion de un numero") {
 	var numero = prompt("Que numero quieres factorizar?");
+	var vlrinicial = 1;
+	for (var cont = 2; cont <= numero; cont++)
+		vlrinicial = vlrinicial * cont;
+	alert("El resultado es: " + vlrinicial);
 }
