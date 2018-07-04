1bdd4fe0a04deb005a492776ab59d2856c0eb2f3
Luciano2792 <adono2002@hotmail.com>
2018-07-03 22:50:51 -0500

    Arregle el pi

--- calculator.js
+++ calculator.js
@@ -4,9 +4,9 @@ if (operacion === "volumen de un cilindro") {
 	var radio = prompt("Cual es el radio del cilindro?");
 	var altura = prompt("Cual es la altura del cilindro?");
 	var exponente = radio *= radio;
-	var resultado_pi = exponente *= 3.141592;
-	var resultado = resultado_pi *= altura;
-	alert("El volumen del cilindro es: " + resultado);
+	var resultado_pi = exponente *= Math.PI;
+	var total = resultado_pi *= altura;
+	alert("El volumen del cilindro es: " + total);
 }
 if (operacion === "transformar de onzas a toneladas") {
 	var onzas = prompt("Cuantas onzas quieres transformar?");
