8ca50f5c8dd4d264d9f8c0e0eb0b08f6191902e4
AngieVL <Luciano2792>
2018-07-06 20:06:50 -0500

    xd

--- calculatortest.js
+++ calculatortest.js
@@ -2,13 +2,28 @@ function askOperation (){
 	question = prompt("Que operacion quieres usar?");
 	return question;
 }
-//askOperation();
 function volCilindro (radio, altura){
-	var radio = prompt("Cual es el radio del cilindro?"); 
-	var altura = prompt("Cual es la altura del cilindro?");
-	var answer = Math.PI * Math.pow(radio, 2) * altura;
+	    answer = Math.PI * Math.pow(radio, 2) * altura;
 	alert ("El volumen del cilindro es: " + answer);
 }
 function onzasToneladas (onzas){
-	
+	var answer = onzas * 0.0000283495;
+	alert(onzas + " onzas equivalen a " + answer + "toneladas");	
+}
+function factorizacion (numero){
+	var vlrinicial = 1;
+	for (var cont = 2; cont <= numero; cont++){
+		vlrinicial = vlrinicial * cont;
+	}
+	alert("El resultado es " + vlrinicial);
+}
+askOperation();
+if(question === "volumen de un cilindro"){
+	volCilindro(prompt("Cual es el radio del cilindro?"), prompt("Cual es la altura del cilindro?"));
+}
+if(question === "transformar de onzas a toneladas"){
+	onzasToneladas(prompt("Inserta el numero de onzas"));
+}
+if(question === "factorizacion de un numero"){
+	factorizacion(prompt("Inserta el numero a factorizar"));
 }
